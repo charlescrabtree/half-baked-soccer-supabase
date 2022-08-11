80c805b804e20c134a7b3ce80935a5c4cf78d0a8
charlescrabtree <charles.crabtree.horn@gmail.com>
2022-08-11 11:43:38 -0700

    missing semicolon.

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = 'https://hxnmyoamqkyeaybcgtum.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh4bm15b2FtcWt5ZWF5YmNndHVtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTk2MzgzNzEsImV4cCI6MTk3NTIxNDM3MX0.M74QbOVkzdaVOrVsLNfuxBZhMbtfI4HGVFx138-KZqI'
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh4bm15b2FtcWt5ZWF5YmNndHVtIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTk2MzgzNzEsImV4cCI6MTk3NTIxNDM3MX0.M74QbOVkzdaVOrVsLNfuxBZhMbtfI4HGVFx138-KZqI';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
